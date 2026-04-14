b40d4d7209d8d23ed8411ed48cc2c585919a11b0
Boris Moreno <boris_marco_moreno@hotmail.com>
2026-04-14 16:29:47 -0500

    fix: size

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -16,6 +16,13 @@ window.addEventListener('orientationchange', () => {
   setTimeout(setVh, 100)
 })
 
+if (window.visualViewport) {
+  window.visualViewport.addEventListener('resize', () => {
+    const vh = window.visualViewport!.height * 0.01
+    document.documentElement.style.setProperty('--vh', `${vh}px`)
+  })
+}
+
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
